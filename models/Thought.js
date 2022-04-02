4f1337af44a478e26e76f938d375646fbc2f7d49
souhila27 <suhamimisou14@gmail.com>
2022-04-02 03:14:25 -0400

    added mongoose models

---
+++ models/Thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+const ReactionSchema = require("./Reaction");
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of friends and replies on retrieval
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// create the Thought model using the ThoughtSchema
+const Thought = model("Thought", ThoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
